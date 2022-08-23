88e5e71ab86211a1a21cc3a015dc6b5e9c60ff2d
zcanfes <zcanfes@gmail.com>
2022-08-23 14:12:24 +0300

    Updates

---
+++ precache-manifest.710b6c56720987bcfcaa115bdc41f276.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d737ba769db033c45cbb00b56edc85db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24f44928342af5e1a0e5",
+    "url": "/static/css/main.74a51ed1.chunk.css"
+  },
+  {
+    "revision": "5a4b08c64fda30f55127",
+    "url": "/static/js/2.3374cc29.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3374cc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f44928342af5e1a0e5",
+    "url": "/static/js/main.9fac950f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "65e47417ccf58990175f56115f49097f",
+    "url": "/static/media/resume.65e47417.pdf"
+  },
+  {
+    "revision": "894b069090ec3e21918f5cdd718b999f",
+    "url": "/static/media/zehranaz.894b0690.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/precache-manifest.710b6c56720987bcfcaa115bdc41f276.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
